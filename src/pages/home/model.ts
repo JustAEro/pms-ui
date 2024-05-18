e45dcaa1c8330a2be33cfe4c62d9f5663fce4f8e
ilyap <ilya.polukhin2002@yandex.ru>
2024-05-19 00:23:07 +0300

    feat: add login modal to home page

--- src/pages/home/model.ts
+++ src/pages/home/model.ts
@@ -1,11 +1,65 @@
-import { createStore } from 'effector';
+import { combine, createEvent, createStore, sample } from 'effector';
+import { not } from 'patronum';
 
 import { UserType } from '@pms-ui/entities/user';
-import { counter } from '@pms-ui/shared/ui';
 import { header as pageHeader } from '@pms-ui/widgets/header';
 
+export const openModal = createEvent();
+export const closeModal = createEvent();
+export const loginEdited = createEvent<string>();
+export const passwordEdited = createEvent<string>();
+export const loginButtonClicked = createEvent();
+const reset = createEvent();
+
+export const $loginModalIsOpened = createStore(false);
+export const $login = createStore('');
+export const $password = createStore('');
+const $isLoginButtonEnabled = combine(
+  $login,
+  $password,
+  (login, password) => login.length > 0 && password.length > 0
+);
+export const $isLoginButtonDisabled = not($isLoginButtonEnabled);
 const $userType = createStore<UserType>(null);
 
-export const counterModel = counter.model.createModel();
-
 export const headerModel = pageHeader.model.createModel({ $userType });
+
+sample({
+  clock: openModal,
+  source: $loginModalIsOpened,
+  filter: (loginModalIsOpened) => !loginModalIsOpened,
+  fn: () => true,
+  target: $loginModalIsOpened,
+});
+
+sample({
+  clock: closeModal,
+  source: $loginModalIsOpened,
+  filter: (loginModalIsOpened) => loginModalIsOpened,
+  fn: () => false,
+  target: $loginModalIsOpened,
+});
+
+sample({
+  clock: loginEdited,
+  target: $login,
+});
+
+sample({
+  clock: passwordEdited,
+  target: $password,
+});
+
+sample({
+  clock: closeModal,
+  target: reset,
+});
+
+sample({
+  clock: reset,
+  target: [
+    $login.reinit,
+    $loginModalIsOpened.reinit,
+    $password.reinit,
+  ] as const,
+});
--- src/pages/home/ui.tsx
+++ src/pages/home/ui.tsx
@@ -1,34 +1,123 @@
+import { useUnit } from 'effector-react';
 import { FC } from 'react';
 
-import { Box, Button, Flex, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Spacer,
+  Text,
+} from '@chakra-ui/react';
 import { header as pageHeader } from '@pms-ui/widgets/header';
 
-import { headerModel } from './model';
+import {
+  $isLoginButtonDisabled,
+  $login,
+  $loginModalIsOpened,
+  $password,
+  closeModal,
+  headerModel,
+  loginButtonClicked,
+  loginEdited,
+  openModal,
+  passwordEdited,
+} from './model';
 
 const textFontSizes = [16, 21, 30];
 
-export const HomePage: FC = () => (
-  <Box>
-    <pageHeader.ui model={headerModel} />
-    <Flex
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      fontSize="3xl"
-    >
-      <Text marginTop="50px" fontWeight="bold" fontSize={textFontSizes}>
-        Добро пожаловать в систему управления проектами
-      </Text>
-      <Button
-        marginTop="50px"
-        colorScheme="teal"
-        bgColor="#319795"
-        width="50%"
-        height="50px"
-        variant="solid"
+export const HomePage: FC = () => {
+  const loginModalIsOpened = useUnit($loginModalIsOpened);
+  const onOpenModal = useUnit(openModal);
+  const onCloseModal = useUnit(closeModal);
+
+  const login = useUnit($login);
+  const password = useUnit($password);
+  const isLoginButtonDisabled = useUnit($isLoginButtonDisabled);
+  const onChangeLogin = useUnit(loginEdited);
+  const onChangePassword = useUnit(passwordEdited);
+
+  const onLoginButtonClick = useUnit(loginButtonClicked);
+
+  return (
+    <Box>
+      <pageHeader.ui model={headerModel} />
+      <Flex
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        fontSize="3xl"
       >
-        Вход
-      </Button>
-    </Flex>
-  </Box>
-);
+        <Text marginTop="50px" fontWeight="bold" fontSize={textFontSizes}>
+          Добро пожаловать в систему управления проектами
+        </Text>
+        <Button
+          onClick={onOpenModal}
+          marginTop="50px"
+          colorScheme="teal"
+          bgColor="#319795"
+          width="50%"
+          height="50px"
+          variant="solid"
+        >
+          Вход
+        </Button>
+        <Modal isOpen={loginModalIsOpened} onClose={onCloseModal}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalCloseButton />
+            <ModalHeader>
+              <Flex alignItems="center" justifyContent="center">
+                <Text fontWeight="bold" fontSize={textFontSizes}>
+                  Вход в аккаунт
+                </Text>
+              </Flex>
+            </ModalHeader>
+            <ModalBody>
+              <Spacer height="20px" />
+              <Flex alignItems="center" justifyContent="center">
+                <Input
+                  value={login}
+                  onChange={(e) => onChangeLogin(e.target.value)}
+                  width="80%"
+                  variant="filled"
+                  placeholder="Логин"
+                />
+              </Flex>
+              <Spacer height="20px" />
+              <Flex alignItems="center" justifyContent="center">
+                <Input
+                  value={password}
+                  onChange={(e) => onChangePassword(e.target.value)}
+                  type="password"
+                  width="80%"
+                  variant="filled"
+                  placeholder="Пароль"
+                />
+              </Flex>
+              <Spacer height="50px" />
+              <Flex alignItems="center" justifyContent="center">
+                <Button
+                  onClick={onLoginButtonClick}
+                  disabled={isLoginButtonDisabled}
+                  width="80%"
+                  variant="solid"
+                  colorScheme="teal"
+                >
+                  Войти
+                </Button>
+              </Flex>
+              <Spacer height="20px" />
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      </Flex>
+    </Box>
+  );
+};
